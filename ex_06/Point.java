83e9d34437e2267109a281195002f929d8eadd92
muizu <muizu0920555@protonmail.com>
2023-05-01 21:59:29 +0900

    change

---
+++ ex_06/Point.java
@@ -0,0 +1,18 @@
+class Point{
+    private int x;
+    private int y;
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public boolean equals(Object obj){//overrideするには型をobjectにしないといけないらしい
+        if (obj == null) return false ;
+        if (obj == this) return true;
+        else{
+            Point other = (Point) obj;
+            return this.x == other.x && this.y == other.y;
+        }
+    }
+}
