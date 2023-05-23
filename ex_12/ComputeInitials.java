d2ded57bdfbe3ec168d170a8a1e5de5bf1bf8541
muizu <muizu0920555@protonmail.com>
2023-05-23 19:14:02 +0900

    change

---
+++ ex_12/ComputeInitials.java
@@ -0,0 +1,7 @@
+class ComputeInitials{
+    public static void main(String[] args){
+        String a = String.valueOf(args[0].charAt(0));
+        String b = String.valueOf(args[1].charAt(0));
+        System.out.println("My initials are: "+a+b);
+    }
+}
